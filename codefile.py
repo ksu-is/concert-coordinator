e2783b5c24c58ee406c132705ea668f8b309768f
haileybeck <123038028+haileybeck@users.noreply.github.com>
2023-04-24 17:45:24 -0400

    Input of musician
    By allowing input of musician, we have the data that is necessary to finish the concert coordination process

--- codefile.py
+++ codefile.py
@@ -1,2 +1,3 @@
 service = input ("What service would you like today?\nMusician inquiry or concert date? : ")
 if service == "musician inquiry":
+ musician = input ("Enter the musician you are seeing in concert:")
