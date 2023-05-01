16cd3b70113f2b93fc7610ac7f17ade695318ca8
haileybeck <123038028+haileybeck@users.noreply.github.com>
2023-05-01 14:28:34 -0400

    Updated code
    Changed welcoming so it can be more inviting on the user's side.

--- codefile.py
+++ codefile.py
@@ -1,3 +1,2 @@
-service = input ("What service would you like today?\nMusician inquiry or concert date? : ")
-if service == "musician inquiry":
- musician = input ("Enter the musician you are seeing in concert:")
+print ("Welcome to Concert Coordinator!")
+service = input ("Are you entering data for a musician (enter 'm') or concert date?(enter 'c')\nMusician or concert date? : ")
