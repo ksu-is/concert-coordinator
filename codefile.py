e8358b3964ca71254cc26fdca2f9a65616a9a2ae
haileybeck <123038028+haileybeck@users.noreply.github.com>
2023-05-01 15:14:29 -0400

    Distinguishing musician
    Added in so the user can log their data about concert

--- codefile.py
+++ codefile.py
@@ -1,2 +1,8 @@
 print ("Welcome to Concert Coordinator!")
 service = input ("Are you entering data for a musician (enter 'm') or concert date?(enter 'c')\nMusician or concert date? : ")
+if service == "m":
+    print ("Please enter the artist you are seeing so we can log it!")
+    musician = input ("Artist:")
+    print ("Perfect! What date are you seeing the musician?")
+    input ("Enter date")
+    print ("Musician and date is now logged for you, thank you!")
